5319bb3cfb20492e46d0686c58859697f9f9cf69
dsreliete <dsreliete@gmail.com>
2016-06-08 16:59:51 -0300

    handling configuration changes

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Court Counter
 This repository contains the Court Counter code of Udacity Course Android for Begginers
+Example handles data and layout on the configuration changes about orientation !!!
--- app/src/main/java/br/com/rodrigues/eliete/courtcounter/MainActivity.java
+++ app/src/main/java/br/com/rodrigues/eliete/courtcounter/MainActivity.java
@@ -1,37 +1,113 @@
 package br.com.rodrigues.eliete.courtcounter;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    public static final String TAG = MainActivity.class.getName();
+    public static final String TEAM_A = "teamA";
+    public static final String TEAM_B = "teamB";
+
     int scoreTeamA = 0;
     int scoreTeamB = 0;
+
+    TextView scoreViewA;
+    TextView scoreViewB;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "on Start");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "on Resume");
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        Log.i(TAG, "on Configuration Change");
+
+        if (newConfig.orientation ==
+                Configuration.ORIENTATION_LANDSCAPE) {
+            setContentView(R.layout.activity_main);
+
+        } else if (newConfig.orientation ==
+                Configuration.ORIENTATION_PORTRAIT){
+            setContentView(R.layout.activity_main);
+
+        }
+
+        scoreViewA = (TextView) findViewById(R.id.team_a_score);
+        scoreViewB = (TextView) findViewById(R.id.team_b_score);
+
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i(TAG, "on Create");
         setContentView(R.layout.activity_main);
+
+        scoreViewA = (TextView) findViewById(R.id.team_a_score);
+        scoreViewB = (TextView) findViewById(R.id.team_b_score);
+
     }
 
-    /**
-     * Displays the given score for Team A.
-     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i(TAG, "on Saved Instance State");
+
+        outState.putInt(TEAM_A, scoreTeamA);
+        outState.putInt(TEAM_B, scoreTeamB);
+
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        scoreTeamA = savedInstanceState.getInt(TEAM_A);
+        scoreTeamB = savedInstanceState.getInt(TEAM_B);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "on Pause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "on Stop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "on Destroy");
+    }
+
     public void displayForTeamA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
-        scoreView.setText(String.valueOf(score));
+        scoreViewA.setText(String.valueOf(score));
     }
 
-    /**
-     * Displays the given score for Team B.
-     */
     public void displayForTeamB(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
-        scoreView.setText(String.valueOf(score));
+        scoreViewB.setText(String.valueOf(score));
     }
 
     public void addThreePointsTeamA(View view){
